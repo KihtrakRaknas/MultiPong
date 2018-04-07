df77c5b08e8c986375c91401dfe42aaca8526cca
KihtrakRaknas <karthikandsankar@gmail.com>
2018-04-07 16:14:01 -0400

    test

--- canvas.js
+++ canvas.js
@@ -1,3 +1,26 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBtLRlcXkrQQTcs-1Qp1Od2rbPoYcwTEHE",
+    authDomain: "sync-test-game.firebaseapp.com",
+    databaseURL: "https://sync-test-game.firebaseio.com",
+    projectId: "sync-test-game",
+    storageBucket: "sync-test-game.appspot.com",
+    messagingSenderId: "825894585819"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+database.ref().set({
+  	//first: false,
+	x:50,
+	y:500,
+	screenWidth: document.body.clientWidth
+  });
+
+
+
 const canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var ballImage = new Image();
@@ -6,11 +29,15 @@ ballImage.src = "ball.png";
 var paddleImage = new Image();
 paddleImage.src = "paddle.png";
 
-var ballY = 50;
-var ballX = 50;
+var ballY = 5;
+var ballX = 5;
 
 var paddleX = 5;
 var paddleY = 5;
+
+var ballVX = 1;
+var ballVY = 1;
+
 
 paddleImage.onload = function(e){
 	console.log("START");
@@ -51,6 +78,30 @@ function start(){
 	canvas.height = window.innerHeight;
 	var loop = setInterval(render,50);
 }
+var MASTER;
+var oppWidth;
+database.ref().once("value", function(e){
+  	if(e.val().first == null || e.val().first == false){
+		database.ref().update({first: true});
+		MASTER=true;
+		paddleX=5;
+	}else{
+		MASTER=false;
+		paddleX=document.body.clientWidth-5;
+	}
+	console.log(MASTER);
+	oppWidth = e.val().screenWidth;
+  });
+
+database.ref().on("value", function(e){
+	if(master){
+  		ballX = e.val().x;
+	}else{
+		ballX = e.val().x-oppWidth;
+	}
+	ballY = e.val().y;
+	renderBall();
+  });
 
 function clear(){
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -66,6 +117,15 @@ function renderPaddle(){
 
 function render(){
 	clear();
-	renderBall();
+	if(MASTER){
+		if(ballY+ballVY<0||ballY+ballVY>window.innerHeight){
+			ballVY=ballVY*-1;
+			console.log("FLIP");
+		}
+		database.ref().update({
+   			x:ballX+ballVX,
+			y:ballY+ballVY
+		});
+	}
 	renderPaddle();	
 }
