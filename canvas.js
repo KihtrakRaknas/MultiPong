33eee0a44a24f3b33b5ed878943ebbe141b1ae8a
KihtrakRaknas <karthikandsankar@gmail.com>
2018-04-07 19:19:51 -0400

    test

--- canvas.js
+++ canvas.js
@@ -95,6 +95,9 @@ database.ref().once("value", function(e){
 		y:500,
 		screenWidth: document.body.clientWidth,
 		VX:10
+		database.ref().update({first: true});
+		MASTER=true;
+		paddleX=5;
  	 });
 	}
 	console.log(MASTER);
